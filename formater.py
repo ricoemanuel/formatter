acb3e9e69c4f30ea899fba77418a50cedccddda3
Emanuel Rico Ruiz <ricoruizemanuel@gmail.com>
2024-07-23 15:08:45 -0500

    sum row addition

--- formater.py
+++ formater.py
@@ -8,19 +8,10 @@ def formatExcel(contentBytes):
     
     df = pd.read_excel(excel, skiprows=5, skipfooter=4, engine='openpyxl')
     
-    # Create a new DataFrame for rows where 'Live Check Amount' is 0
     df_zero = pd.DataFrame(columns=df.columns)
-    
-    for index, row in df.iterrows():
-        if row['Live Check Amount'] == 0:
-            # Check if the client is already in the df_zero DataFrame
-            if row['Client'] not in df_zero['Client'].values:
-                # Append the row to df_zero DataFrame
-                df_zero = df_zero.append(row)
-    
-    df = df[df['Live Check Amount'] > 0]
+  
     grouped_data = df.groupby('Client').agg(
-        number_of_live_checks=pd.NamedAgg(column='Live Check Amount', aggfunc='count'),
+        number_of_live_checks=pd.NamedAgg(column='Live Check Amount', aggfunc=count_non_zero),
         check_totals=pd.NamedAgg(column='Live Check Amount', aggfunc='sum')
     ).reset_index()
 
@@ -29,6 +20,7 @@ def formatExcel(contentBytes):
         'number_of_live_checks': grouped_data['number_of_live_checks'].sum(),
         'check_totals': grouped_data['check_totals'].sum()
     }
+    
 
     output = BytesIO()
     with pd.ExcelWriter(output, engine='openpyxl') as writer:
@@ -37,3 +29,6 @@ def formatExcel(contentBytes):
     output.seek(0)
 
     return output, df_zero
+
+def count_non_zero(series):
+    return (series != 0).sum()
