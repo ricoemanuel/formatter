0e397953e82f813454482b063d3ab4d798ef5dce
Emanuel Rico Ruiz <ricoruizemanuel@gmail.com>
2024-07-23 14:59:34 -0500

    sum row addition

--- formater.py
+++ formater.py
@@ -7,6 +7,17 @@ def formatExcel(contentBytes):
     excel = BytesIO(decoded)
     
     df = pd.read_excel(excel, skiprows=5, skipfooter=4, engine='openpyxl')
+    
+    # Create a new DataFrame for rows where 'Live Check Amount' is 0
+    df_zero = pd.DataFrame(columns=df.columns)
+    
+    for index, row in df.iterrows():
+        if row['Live Check Amount'] == 0:
+            # Check if the client is already in the df_zero DataFrame
+            if row['Client'] not in df_zero['Client'].values:
+                # Append the row to df_zero DataFrame
+                df_zero = df_zero.append(row)
+    
     df = df[df['Live Check Amount'] > 0]
     grouped_data = df.groupby('Client').agg(
         number_of_live_checks=pd.NamedAgg(column='Live Check Amount', aggfunc='count'),
@@ -25,4 +36,4 @@ def formatExcel(contentBytes):
     
     output.seek(0)
 
-    return output
+    return output, df_zero
