e89b34391a74a8e6e42a07a84a13369882f5ff6a
Emanuel Rico Ruiz <ricoruizemanuel@gmail.com>
2024-07-23 12:49:26 -0500

    sum row addition

--- formater.py
+++ formater.py
@@ -7,9 +7,9 @@ def formatExcel(contentBytes):
     excel = BytesIO(decoded)
     
     df = pd.read_excel(excel, skiprows=5, skipfooter=4, engine='openpyxl')
-
+    df = df[df['Live Check Amount'] > 0]
     grouped_data = df.groupby('Client').agg(
-        number_of_live_checks=pd.NamedAgg(column='Live Check Amount', aggfunc='sum'),
+        number_of_live_checks=pd.NamedAgg(column='Live Check Amount', aggfunc='count'),
         check_totals=pd.NamedAgg(column='Live Check Amount', aggfunc='sum')
     ).reset_index()
 
