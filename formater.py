02448cfd4de7d1e2019db2e98f643c62d34675b3
Emanuel Rico Ruiz <ricoruizemanuel@gmail.com>
2024-07-23 15:42:57 -0500

    sort by Client

--- formater.py
+++ formater.py
@@ -8,11 +8,11 @@ def formatExcel(contentBytes):
     
     df = pd.read_excel(excel, skiprows=5, skipfooter=4, engine='openpyxl')
     
-    df_zero=df[df['Live Check Amount'] == 0]
-    df_zero=df_zero.groupby('Client').agg(
-        number_of_live_checks=pd.NamedAgg(column='Live Check Amount', aggfunc='sum'),
-        check_totals=pd.NamedAgg(column='Live Check Amount', aggfunc='sum')
-    ).reset_index()
+    # df_zero=df[df['Live Check Amount'] == 0]
+    # df_zero=df_zero.groupby('Client').agg(
+    #     number_of_live_checks=pd.NamedAgg(column='Live Check Amount', aggfunc='sum'),
+    #     check_totals=pd.NamedAgg(column='Live Check Amount', aggfunc='sum')
+    # ).reset_index()
 
     df = df[df['Live Check Amount'] > 0]
     grouped_data = df.groupby('Client').agg(
@@ -20,10 +20,10 @@ def formatExcel(contentBytes):
         check_totals=pd.NamedAgg(column='Live Check Amount', aggfunc='sum')
     ).reset_index()
 
-    df_concat = pd.concat([df_zero, grouped_data])
+    # df_concat = pd.concat([df_zero, grouped_data])
 
-    grouped_data=df_concat.drop_duplicates(subset='Client', keep='last', inplace=False)
-    grouped_data = grouped_data.sort_values('Client', ascending=True)
+    # grouped_data=df_concat.drop_duplicates(subset='Client', keep='last', inplace=False)
+    # grouped_data = grouped_data.sort_values('Client', ascending=True)
     grouped_data.loc[len(grouped_data)]={
         'Client': 'Totals',
         'number_of_live_checks': grouped_data['number_of_live_checks'].sum(),
