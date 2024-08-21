b7e3a2454e150e931a44bfa22b4337ce84492c4e
Emanuel Rico Ruiz <ricoruizemanuel@gmail.com>
2024-08-21 09:52:44 -0500

    new endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, jsonify, request, send_file
+from flask import Flask, Response, jsonify,request, send_file
 from formater import formatExcel, formatFromJson, discrepancies_report
 from flask_cors import CORS
 
@@ -7,34 +7,32 @@ CORS(app)
 
 @app.route('/format', methods=['POST'])
 def format():
-    if request.content_type != 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-        return jsonify({"error": "Unsupported Media Type"}), 415
+    contentBytes = request.get_data()
 
-    try:
-        contentBytes = request.get_data()
-        content = formatExcel(contentBytes)
-        return send_file(content, download_name='file.xlsx', as_attachment=True, mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    content=formatExcel(contentBytes)
+    
+    return send_file(content,download_name='file.xlsx', as_attachment=True, mimetype="application/vnd.openxmlformats-officedocument.spreadsheet.sheet")
 
 @app.route('/format-json', methods=['POST'])
 def format_json():
-    if request.content_type != 'application/json':
-        return jsonify({"error": "Unsupported Media Type"}), 415
-
     data = request.get_json()
     response = formatFromJson(data)
     return response
 
 @app.route('/discrepancies', methods=['POST'])
 def discrepancies():
-    if request.content_type != 'application/json':
-        return jsonify({"error": "Unsupported Media Type"}), 415
-
+    # Obtener el contenido de la solicitud POST
     data = request.get_json()
-    content = data.get('$content')
+    
+    # Extraer el contenido de $content
+    content = data.get('contentBytes')
+    
+    # Procesar el contenido
     processed_content = discrepancies_report(content)
-    return send_file(processed_content, download_name='file.xlsx', as_attachment=True, mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    
+    return send_file(processed_content,download_name='file.xlsx', as_attachment=True, mimetype="application/vnd.openxmlformats-officedocument.spreadsheet.sheet")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
